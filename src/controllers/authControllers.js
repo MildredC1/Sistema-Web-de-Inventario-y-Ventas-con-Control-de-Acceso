42da8aaf549713f90b7f27b67d6dc2bd0f92c473
castilloBrayan <ordonezcastillo20@gmail.com>
2025-10-21 12:54:29 -0600

    Correccion al validar usuario en authController.js

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,22 +1,53 @@
 import bcrypt from "bcrypt"
 import pool from "../config/db.js"
 
-exports.login = async (req, res) => {
+// muestra el Login (GET)
+export function showLogin(req, res) {
+    res.render("login", {title: "Iniciar sesión"})
+}
+
+// procesar el inicio de sesión (POST)
+export async function login(req, res) {
   const { correo, contrasena } = req.body;
-  const [rows] = await pool.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
-  const usuario = rows[0];
 
-  if (usuario && await bcrypt.compare(contrasena, usuario.contrasena)) {
-    req.session.user = usuario;
-
-    if (usuario.admin) {
-      res.redirect('/'); // Acceso completo
-    } else {
-      res.redirect('/'); // Solo visualización
-    }
-  } else {
-    res.render('mensaje', { 
-      titulo: 'Error',
-      mensaje: 'Datos invalidos'});
+  if (!email || !password) {
+        return  res.status(400).render("mensaje", {
+          titulo: "Iniciar sesión", 
+          mensaje: "Todos los campos son obligatorios"})
   }
+  
+  try {
+    // buscar el usuario en la BD
+    const [rows] = await pool.query(
+      'SELECT * FROM usuarios WHERE correo = ?', [correo]);
+    
+    // no existe el usuario
+    if (rows.length === 0) {
+      return res.status(401).render("mensaje", {
+        titulo: "Iniciar sesión", 
+        mensaje: "Credenciales inválidas"})
+    }
+
+    const usuario = rows[0];
+
+    const iguales = await bcrypt.compare(contrasena, usuario.contrasena)
+    if (!iguales) {
+      return res.status(401).render("mensaje", {
+        titulo: "Iniciar sesión",
+        mensaje: "Credenciales inválidas"})
+
+    } else {
+      req.session.user = usuario;
+
+      if (usuario.admin) {
+        res.redirect('/'); // Acceso completo
+      } else {
+        res.redirect('/'); // Solo visualización
+      }
+    }
+
+  } catch (error) {
+    
+  }
+  
 };
