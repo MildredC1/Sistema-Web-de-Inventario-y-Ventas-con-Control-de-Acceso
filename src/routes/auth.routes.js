ac898934c77f5be10ea6b029a13f01602c33aeda
castilloBrayan <ordonezcastillo20@gmail.com>
2025-10-21 12:00:28 -0600

    Implementar ruta /login (GET y POST): showLogin -> mostrarLogin

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,7 +7,7 @@ import { redirectAuth } from '../middlewares/auth.js';
 const router = Router()
 
 // Rutas relacionadas con 'Login'
-router.get('/login', redirectAuth, auth.showLogin)
+router.get('/login', redirectAuth, auth.mostrarLogin)
 router.post('/login', auth.login)
 
 export default router
