227b5a7a9e1c61c8365c92a2bfff8b53bd8807d6
Mildred Cop <copmildred@gmail.com>
2025-10-22 14:19:11 -0600

    Se creo modelo y consulta con JOIN de productos y usuarios

--- src/models/productos.model.js
+++ src/models/productos.model.js
@@ -6,12 +6,37 @@ const ProductosModel = {
     mysql.query(query, (error, resultados) => db(error, resultados))
   },
 
-  crear({id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento}, cb){
+
+  listarConUsuarios(cb) {
+    const query = `
+        SELECT 
+          productos.id,
+          productos.nombre,
+          productos.categoria,
+          productos.marca,
+          productos.precio,
+          productos.stock,
+          productos.proveedor_email,
+          productos.rating,
+          productos.creado_es,
+          productos.descuento,
+          usuarios.id AS usuario_id,
+          usuarios.nombre AS usuario_nombre,
+          usuarios.correo AS usuario_correo,
+          usuarios.admin AS usuario_admin
+        FROM productos
+        JOIN usuarios ON productos.usuario_id = usuarios.id
+        ORDER BY productos.id DESC`;
+        mysql.query(query, (error, resultados) => cb(error, resultados));
+      },
+
+
+  crear({ nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento,usuario_id}, cb){
         const query = `
-            INSERT INTO productos (id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento) 
+            INSERT INTO productos (nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento,usuario_id) 
             VALUES (?,?,?,?,?,?,?,?,?)`
 
-        const parametros = [id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento]
+        const parametros = [nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento,usuario_id]
 
         mysql.query(query, parametros, (err, results) => cb(err, results))
   },
