bb662d612ebe40efded62839fb9e86e353fa1543
castilloBrayan <ordonezcastillo20@gmail.com>
2025-10-22 12:07:12 -0600

    Crear funciones para productosModel Crear, Editar y Eliminar, y modificacion de formato en controller

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,8 +1,8 @@
 const Productos=require("../models/productosModels.js")
 
-const ProductosController={
+const ProductosController = {
   listar(req,res){
-    Productos.listar((error,productos)=>{
+    Productos.listar((error,productos) => {
       if(error){
         return res.render("error", {
         titulo: "No se encontraron Productos",
--- src/models/productosModels.js
+++ src/models/productosModels.js
@@ -2,9 +2,39 @@ const mysql = require('../config/db.js')
 
 const ProductosModel = {
   listar(db) {
-    const query = `SELECT id, nombre, categoria, marca, precio, stock, proveedor_email, rating,   creado_es, descuento FROM productos  ORDER BY id DESC`
+    const query = `SELECT id, nombre, categoria, marca, precio, stock, proveedor_email, rating, creado_es, descuento FROM productos  ORDER BY id DESC`
     mysql.query(query, (error, resultados) => db(error, resultados))
-  }
+  },
+
+  crear({id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento}, cb){
+        const query = `
+            INSERT INTO productos (id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento) 
+            VALUES (?,?,?,?,?,?,?,?,?)`
+
+        const parametros = [id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento]
+
+        mysql.query(query, parametros, (err, results) => cb(err, results))
+  },
+  
+  editar({id, nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento}, cb){
+    const query = `
+      UPDATE productos
+      SET nombre = ?, categoria = ?, marca = ?, precio = ?, stock = ?, proveedor_email = ?, rating = ?, descuento = ?
+      WHERE id = ?`
+    
+    const parametros = [nombre, categoria, marca, precio, stock, proveedor_email, rating, descuento, id]
+
+    mysql.query(query, parametros, (err, results) => cb(err, results))
+  },
+
+  eliminar(id, cb) {
+        const query = `
+            DELETE FROM productos
+            WHERE id = ?`
+
+            mysql.query(query, [id], (err, results) => cb(err, results))
+    }
+
 }
 
 module.exports = ProductosModel
