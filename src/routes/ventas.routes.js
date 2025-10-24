125a272edaffc1f1d9c16dacbbf9952f7fd838d7
Mildred Cop <copmildred@gmail.com>
2025-10-24 10:33:10 -0600

    Se modifico el metodo de registrar por procesar en router.post, y se agrego / en router get(/crear venta)

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -7,8 +7,8 @@ const router = Router()
 
 // Rutas relacionadas con 'Ventas'
 router.get("/ventas", requiereAuth, ventasController.mostrar)   // Mostrar formulario y listado Ventas
-router.post("/ventas", requiereAuth, ventasController.registrar) // Procesar registro Ventas
+router.post("/ventas", requiereAuth, ventasController.procesar) // Procesar registro Ventas
 
-router.get("crear_venta", requiereAuth, ventasController.mostrarFormulario) // Nos muestra el formulario para crear producto
+router.get("/crear_venta", requiereAuth, ventasController.mostrarFormulario) // Nos muestra el formulario para crear producto
 
 export default router
