8e047efecdb86d7efa8c33fa1d343c54ea845764
castilloBrayan <ordonezcastillo20@gmail.com>
2025-10-24 10:13:39 -0600

    agregacion de middlewares necesarios

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ import ventasRoutes from './src/routes/ventas.routes.js'
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
+// middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://${HOST}:${PORT}`);
 });
